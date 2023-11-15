fd2fe3911136039e4314f91b8ed9ea5ca1b9a972
Javier Pérez Arteaga <surtich@gmail.com>
2023-11-15 13:24:29 +0100

    Implementado writeGreetingSafe, que garantiza que se cierre el fichero aunque se produzca un error de ejecución

--- src/Book.hs
+++ src/Book.hs
@@ -6,6 +6,7 @@ import Relude
 import qualified System.Directory as Dir
 import System.FilePath ((</>))
 import qualified System.IO as IO
+import Control.Monad.Trans.Resource (allocate, runResourceT)
 
 getDataDir :: IO FilePath
 getDataDir = do
@@ -20,3 +21,13 @@ writeGreetingFile = do
     IO.hPutStrLn h "hello"
     IO.hPutStrLn h "world"
     IO.hClose h
+
+writeGreetingSafe :: IO ()
+-- @IO es un type application y permite informar a GHCI que el tipo base es IO. Se puede eliminar si de define el tipo de la función
+writeGreetingSafe = runResourceT @IO do
+    dir <- liftIO getDataDir
+    (_releaseKey, h) <- allocate
+        (IO.openFile (dir </> "greeting-safe.txt") WriteMode)
+        IO.hClose
+    liftIO (IO.hPutStrLn h "hello")
+    liftIO (IO.hPutStrLn h "world")
