63013e8481e00464827989c464211d08e4c02f66
Javier Pérez Arteaga <surtich@gmail.com>
2023-11-15 12:29:10 +0100

    Se escribe unsaludo en un fichero

--- src/Book.hs
+++ src/Book.hs
@@ -5,9 +5,18 @@ import Relude
 
 import qualified System.Directory as Dir
 import System.FilePath ((</>))
+import qualified System.IO as IO
 
 getDataDir :: IO FilePath
 getDataDir = do
     dir <- Dir.getXdgDirectory Dir.XdgData "sockets-and-pipes"
     Dir.createDirectoryIfMissing True dir
     return dir
+
+writeGreetingFile :: IO ()
+writeGreetingFile = do
+    dir <- getDataDir
+    h <- IO.openFile (dir </> "greeting.txt") WriteMode
+    IO.hPutStrLn h "hello"
+    IO.hPutStrLn h "world"
+    IO.hClose h
